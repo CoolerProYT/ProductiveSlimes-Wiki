cf86c713db2e44bad92305e59b13b781af0e9abd
CoolerProRB <118531502+CoolerProRB@users.noreply.github.com>
2024-11-19 20:00:31 +0800

    updated changelog

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -27,8 +27,8 @@
             <div class="mt-5">
                 <h1 class="text-xl md:text-3xl font-bold">Current Version Support</h1>
                 <ul class="list-disc ms-4">
-                    <li>1.21.3</li>
                     <li>1.21.1</li>
+                    <li>1.21.3</li>
                 </ul>
             </div>
 
@@ -37,34 +37,29 @@
                 <ul class="list-disc ms-4">
                     <li class="font-bold text-xl">1.21.3</li>
                     <ul class="ps-3" style="list-style-type: square">
-                        <li class="font-bold text-lg">v1.1.2 (Upcoming Update) ?</li>
-                        <ul>
-                            <li>- Added back JEI (Waiting for JEI to port 1.21.3)</li>
-                        </ul>
-
-                        <li class="font-bold text-lg">v1.1.1</li>
-                        <ul>
+                        <li class="font-bold text-lg">v1.1.1 <span class="cursor-pointer select-none" id="v111-1213">+</span></li>
+                        <ul id="v111-1213-log" style="display: none">
                             <li>- Initial port to 1.21.3</li>
                         </ul>
                     </ul>
 
-                    <li class="font-bold text-xl mt-3 border-t">1.21.1</li>
+                    <li class="font-bold text-xl mt-3 py-3 border-t">1.21.1</li>
                     <ul class="ps-3" style="list-style-type: square">
-                        <li class="font-bold text-lg">v1.1.2</li>
-                        <ul>
+                        <li class="font-bold text-lg">v1.1.2 <span class="cursor-pointer select-none" id="v112-1211">+</span></li>
+                        <ul id="v112-1211-log" style="display: none">
                             <li>- Added squish animation for slime</li>
                         </ul>
 
-                        <li class="font-bold text-lg">v1.1.1</li>
-                        <ul>
+                        <li class="font-bold text-lg">v1.1.1 <span class="cursor-pointer select-none" id="v111-1211">+</span></li>
+                        <ul id="v111-1211-log" style="display: none">
                             <li>- Slime will drop slimeball on death now</li>
                             <li>- Added <b>dna_item</b> tag to DNA</li>
                             <li>- Added missing tag for some slimeball</li>
                             <li>- Changed 2 input slot in <b>DNA Synthesizer</b> to accept <b>dna_item</b> tag only</li>
                         </ul>
 
-                        <li class="font-bold text-lg">v1.1.0</li>
-                        <ul>
+                        <li class="font-bold text-lg">v1.1.0 <span class="cursor-pointer select-none" id="v110-1211">+</span></li>
+                        <ul id="v110-1211-log" style="display: none">
                             <li>- Added <b>Energy Slime</b>, <b>Slimeball</b>, and <b>Slime Block</b></li>
                             <li>- Implemented <b>FE</b> to <b>Soliding Station</b> and <b>Melting Station</b></li>
                             <li>- Added <b>Cable</b> and <b>Energy Generator</b></li>
@@ -81,30 +76,30 @@
                             <li>- Updated <b>Guidebook</b> (For more complete information please use <b>JEI</b>)</li>
                         </ul>
 
-                        <li class="font-bold text-lg">v1.0.3</li>
-                        <ul>
+                        <li class="font-bold text-lg">v1.0.3 <span class="cursor-pointer select-none" id="v103-1211">+</span></li>
+                        <ul id="v103-1211-log" style="display: none">
                             <li>- Added <b>slimeball</b> tag to all slimeball variants</li>
                             <li>- Changed vanilla recipe that uses slimeball to accept every slimeball variant</li>
                             <li>- Changed particle</li>
                             <li>- Added many new variants of slimes, check it out in <b>Guidebook</b></li>
                         </ul>
 
-                        <li class="font-bold text-lg">v1.0.2</li>
-                        <ul>
+                        <li class="font-bold text-lg">v1.0.2 <span class="cursor-pointer select-none" id="v102-1211">+</span></li>
+                        <ul id="v102-1211-log" style="display: none">
                             <li>- Added <b>Slime Block</b> for all tiers of slime</li>
                             <li>- Added melting recipe for all <b>Slime Block</b> tiers</li>
                             <li>- Slime able to split on death</li>
                         </ul>
 
-                        <li class="font-bold text-lg">v1.0.1</li>
-                        <ul>
+                        <li class="font-bold text-lg">v1.0.1 <span class="cursor-pointer select-none" id="v101-1211">+</span></li>
+                        <ul id="v101-1211-log" style="display: none">
                             <li>- Fixed <b>Liquid Soliding Station</b> translation</li>
                             <li>- Fixed <b>Soliding Station</b> capabilities</li>
                             <li>- Prevented slime from despawning when player is far away</li>
                         </ul>
 
-                        <li class="font-bold text-lg">v1.0.0</li>
-                        <ul>
+                        <li class="font-bold text-lg">v1.0.0 <span class="cursor-pointer select-none" id="v100-1211">+</span></li>
+                        <ul id="v100-1211-log" style="display: none">
                             <li>- Initial Release</li>
                         </ul>
 
@@ -116,7 +111,17 @@
 </template>
 
 <script>
+import $ from 'jquery';
 
+export default {
+    mounted() {
+        $('span').click(function() {
+            var id = $(this).attr('id');
+            $(this).text() === '+' ? $(this).text('-') : $(this).text('+');
+            $('#' + id + "-log").slideToggle();
+        });
+    }
+}
 </script>
 
 <style>
